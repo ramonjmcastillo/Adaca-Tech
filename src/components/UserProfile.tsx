6f4b20e52edffb118d8f7bebb369a1ee2a9dc262
Josh Castillo <ramonjmcastillo@gmail.com>
2025-08-14 21:07:24 +0800

    Refactored Contact Section for User Profile

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -3,9 +3,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import { Divider } from "primereact/divider";
-import { Tag } from "primereact/tag";
 import { Panel } from "primereact/panel";
-import { Fieldset } from "primereact/fieldset";
+import { UserContact, UserCompany, UserAddress } from "./UserProfileSections";
 
 import type { User } from "../types/user";
 
@@ -61,46 +60,18 @@ const UserProfile: React.FC = () => {
       <Card
         title={user.name}
         className="mb-3"
-        subTitle={<Tag value={user.username} severity="info" />}
+        subTitle={<span>{user.username}</span>}
       >
         <Divider align="left">
-          <Tag value="Contact" severity="info" />
+          <UserContact user={user} />
         </Divider>
-        <Fieldset
-          className="mb-3"
-          legend={<span className="hidden">Contact</span>}
-          toggleable={false}
-        >
-          <Tag icon="pi pi-envelope" value={user.email} className="mr-2" />
-          <Tag icon="pi pi-phone" value={user.phone} className="mr-2" />
-          <Tag icon="pi pi-globe" value={user.website} />
-        </Fieldset>
         <Divider align="left">
-          <Tag value="Company" severity="info" />
+          <UserCompany user={user} />
         </Divider>
-        <Panel header={user.company.name} className="mb-3">
-          <Fieldset
-            legend={<span className="hidden">Company</span>}
-            toggleable={false}
-          >
-            <span>{user.company.catchPhrase}</span>
-            <span className="text-sm text-color-secondary">
-              {user.company.bs}
-            </span>
-          </Fieldset>
-        </Panel>
+        <Panel header={user.company.name} className="mb-3"></Panel>
         <Divider align="left">
-          <Tag value="Address" severity="info" />
+          <UserAddress user={user} />
         </Divider>
-        <Panel className="mb-3">
-          <Fieldset
-            legend={<span className="hidden">Address</span>}
-            toggleable={false}
-          >
-            {user.address.street}, {user.address.suite},<br />
-            {user.address.city}, {user.address.zipcode}
-          </Fieldset>
-        </Panel>
       </Card>
       <Button
         label="Back to User List"
---
+++ src/components/UserProfileSections.tsx
@@ -0,0 +1,50 @@
+import { Tag } from "primereact/tag";
+import { Fieldset } from "primereact/fieldset";
+import type { User } from "../types/user";
+
+export function UserContact({ user }: { user: User }) {
+  return (
+    <>
+      <Tag value="Contact" severity="info" />
+      <Fieldset
+        className="mb-3"
+        legend={<span className="hidden">Contact</span>}
+        toggleable={false}
+      >
+        <Tag icon="pi pi-envelope" value={user.email} className="mr-2" />
+        <Tag icon="pi pi-phone" value={user.phone} className="mr-2" />
+        <Tag icon="pi pi-globe" value={user.website} />
+      </Fieldset>
+    </>
+  );
+}
+
+export function UserCompany({ user }: { user: User }) {
+  return (
+    <>
+      <Tag value="Company" severity="info" />
+      <Fieldset
+        legend={<span className="hidden">Company</span>}
+        toggleable={false}
+      >
+        <span>{user.company.catchPhrase}</span>
+        <span className="text-sm text-color-secondary">{user.company.bs}</span>
+      </Fieldset>
+    </>
+  );
+}
+
+export function UserAddress({ user }: { user: User }) {
+  return (
+    <>
+      <Tag value="Address" severity="info" />
+      <Fieldset
+        legend={<span className="hidden">Address</span>}
+        toggleable={false}
+      >
+        {user.address.street}, {user.address.suite},<br />
+        {user.address.city}, {user.address.zipcode}
+      </Fieldset>
+    </>
+  );
+}
